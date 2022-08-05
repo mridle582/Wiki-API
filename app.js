3e1bd8b9900e106f5d8f480c300bf11d4d15fd20
mridle582 <mridle582@gmail.com>
2022-08-05 00:17:39 -0400

    write app boilerplate

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+// mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
