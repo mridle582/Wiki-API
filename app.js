b541d9c4f1f06100680a9d882a8421c8916862ed
mridle582 <mridle582@gmail.com>
2022-08-05 15:07:50 -0400

    add post & delete routes for /articles

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get("/articles", (req, res) => {
     });
 });
 
+app.post("/articles", (req, res) => {
+    const reqTitle = req.body.title;
+    const reqContent = req.body.content;
+    // console.log({reqTitle, reqContent});
+    const reqArticle = new Article({
+        title: reqTitle,
+        content: reqContent
+    });
+    reqArticle.save((err) => {
+       err ? res.send(err) : res.send("Successfully added new Article.");
+    });
+});
+
+app.delete("/articles", (req, res) => {
+    Article.deleteMany({}, (err) => {
+        err ? res.send(err) : res.send("Successfully deleted all Articles.");
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
