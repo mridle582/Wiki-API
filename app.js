acc1f33d6352442b1559e288c316d9f7a1b2f4ba
mridle582 <mridle582@gmail.com>
2022-08-05 15:30:36 -0400

    refactor /articles route methods

--- app.js
+++ app.js
@@ -22,30 +22,29 @@ const articlesSchema = {
 
 const Article = mongoose.model("Article", articlesSchema);
 
-app.get("/articles", (req, res) => {
-    Article.find({}, (err, foundArticles) => {
-        err ? res.send(err) : res.send(foundArticles);
-    });
-});
-
-app.post("/articles", (req, res) => {
-    const reqTitle = req.body.title;
-    const reqContent = req.body.content;
-    // console.log({reqTitle, reqContent});
-    const reqArticle = new Article({
-        title: reqTitle,
-        content: reqContent
-    });
-    reqArticle.save((err) => {
-       err ? res.send(err) : res.send("Successfully added new Article.");
-    });
-});
-
-app.delete("/articles", (req, res) => {
-    Article.deleteMany({}, (err) => {
-        err ? res.send(err) : res.send("Successfully deleted all Articles.");
-    });
-});
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({}, (err, foundArticles) => {
+            err ? res.send(err) : res.send(foundArticles);
+        });
+    })
+    .post((req, res) => {
+        const reqTitle = req.body.title;
+        const reqContent = req.body.content;
+        // console.log({reqTitle, reqContent});
+        const reqArticle = new Article({
+            title: reqTitle,
+            content: reqContent
+        });
+        reqArticle.save((err) => {
+           err ? res.send(err) : res.send("Successfully added new Article.");
+        });
+    })
+    .delete((req, res) => {
+        Article.deleteMany({}, (err) => {
+            err ? res.send(err) : res.send("Successfully deleted all Articles.");
+        });
+    })
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
