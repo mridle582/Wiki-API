5ca3acfe7ea0e3e6d093fa83fda4932264dfa3d8
mridle582 <mridle582@gmail.com>
2022-08-05 16:11:13 -0400

    add GET route for specific article

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
 app.use(express.static("public"));
@@ -22,26 +22,58 @@ const articlesSchema = {
 
 const Article = mongoose.model("Article", articlesSchema);
 
+
+/////////////////////////All Articles///////////////////////////////////
+
+
 app.route("/articles")
+
     .get((req, res) => {
         Article.find({}, (err, foundArticles) => {
             err ? res.send(err) : res.send(foundArticles);
         });
     })
+
     .post((req, res) => {
         const reqArticle = new Article({
             title: req.body.title,
             content: req.body.content
         });
         reqArticle.save((err) => {
-           err ? res.send(err) : res.send("Successfully added new Article.");
+            err ? res.send(err) : res.send("Successfully added new Article.");
         });
     })
+
     .delete((req, res) => {
         Article.deleteMany({}, (err) => {
             err ? res.send(err) : res.send("Successfully deleted all Articles.");
         });
+    });
+
+
+/////////////////////////Individual Articles///////////////////////////////////
+
+
+app.route("/articles/:articleName")
+
+    .get((req, res) => {
+        const artName = req.params.articleName;
+        Article.findOne({
+            title: artName
+        }, (err, foundDoc) => {
+            if (!err) {
+                if (!foundDoc) {
+                    res.send(`Oh no! No Articles with name \"${artName}\" in \
+                        Database. Please check your spelling and try again.`);
+                } else {
+                    res.send(foundDoc);
+                }
+            } else {
+                res.send(err);
+            }
+        });
     })
+
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
